1461c3b5d58f273603c136c6146c6207245b3a24
MoonStyIe <mvjvl1793@naver.com>
2023-04-24 10:17:46 +0900

    file updated

--- app.py
+++ app.py
@@ -2,7 +2,37 @@
 import streamlit as st
 
 def main():
+    # Title
     st.title("Hello World")
+
+    # Text
+    st.text('This is so {}' .format('good'))
+
+    # Header
+    st.header("This is Header")
+
+    # Subheader
+    st.subheader('This is subHeader')
+
+    # Markdown
+    st.markdown('## This is Markdown')
+
+    # 색상이 들어간 텍스트 feature
+    st.success('성공')
+    st.warning('경고')
+    st.info('정보와 관련된 탭')
+    st.error('에러 메시지')
+    st.exception('예외처리')
+
+    # st.write()
+    st.write('일반텍스트')
+    st.write(1+2)
+    st.write(dir(str))
+    st.title(':exclamation:')
+
+    # Help
+    st.help(range)
+    st.help(st.title)
 
 if __name__ == '__main__':
     main()
